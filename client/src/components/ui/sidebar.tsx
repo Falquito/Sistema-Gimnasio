191ec0f386e5aaaddbb62d6f3a87b3ee4c05c6d1
imleticio <mleonel.martinez0098@gmail.com>
2025-09-25 23:30:38 -0300

    comit de prueba 18

--- client/src/components/ui/sidebar.tsx
+++ client/src/components/ui/sidebar.tsx
@@ -89,7 +89,7 @@ export const DesktopSidebar = ({
     <>
       <motion.div
         className={cn(
-          "h-full px-4 py-4 hidden  md:flex md:flex-col text-black bg-neutral-900 dark:bg-[#101010] w-[300px] shrink-0   ",
+          "h-full px-4 py-4 hidden  md:flex md:flex-col text-black bg-neutral-100 dark:bg-white w-[300px] shrink-0   ",
           className
         )}
         animate={{
